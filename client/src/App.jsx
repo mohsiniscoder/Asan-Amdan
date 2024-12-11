221491f3880ef8805c92c17e18b205266e17db5b
HP EliteBook 840 G5 <mohsinbhai894@gmail.com>
2024-12-11 12:30:10 +0500

    demo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import Navbar from './components/Navbar.jsx'; // Assuming Navbar.jsx is in a components folder
+import Navbar from './components/Navbar';
 
 const App = () => {
-  return (
-    <>
-      <h1>Sab ko milega Rozagr :) </h1>
-    </>
-  );
+    const links = [
+        { href: '/provide-service', text: 'Provide Service' },
+        { href: '/order-service', text: 'Order Service' },
+        { href: '/be-a-manager', text: 'Be a Manager' },
+    ];
+
+    const buttons = [
+        { text: 'Sign In', className: 'btn-primary', onClick: () => console.log('Sign In clicked') },
+        { text: 'Join', className: 'btn-success', onClick: () => console.log('Join clicked') },
+    ];
+
+    return (
+        <Navbar 
+            logoText="Asan Rozgar" 
+            logoHref="/" 
+            links={links} 
+            buttons={buttons} 
+        />
+    );
 };
 
 export default App;
