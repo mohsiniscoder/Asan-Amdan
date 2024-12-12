ec7107f18f8b51df360f6529ff4dda1354daf6da
Awais Ashraf <mhawais504@gmail.com>
2024-12-12 10:47:32 +0500

    Added Gig section with some good practices and divide code into separte files

---
+++ client/src/data/links.js
@@ -0,0 +1,11 @@
+export const links = [
+    { href: "/provide-service", text: "Provide Service" },
+    { href: "/order-service", text: "Order Service" },
+    { href: "/be-a-manager", text: "Be a Manager" },
+  ];
+  
+  export const buttons = [
+    { text: "Sign In", className: "btn-primary", onClick: () => console.log("Sign In clicked") },
+    { text: "Join", className: "btn-success", onClick: () => console.log("Join clicked") },
+  ];
+  
