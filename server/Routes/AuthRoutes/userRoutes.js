8c7bf3764ef6fcdd431174a5e21686713eb63846
MahrAbrarThind <mahrabrarthind@gmail.com>
2024-12-23 03:39:15 +0500

    changed a little in userroutes

--- server/Routes/AuthRoutes/userRoutes.js
+++ server/Routes/AuthRoutes/userRoutes.js
@@ -1,10 +1,20 @@
 import express from "express"
 import { loginController, registerController } from "../../Controllers/AuthControllers/userControllers.js";
+import { checkUser } from "../../Middlewares/authMiddlewares.js";
 const router=express.Router();
 
 router.post("/register",registerController);
 router.post("/login",loginController);
 
+
+// checking user
+router.post("/chekuser", checkUser, (req, res) => {
+    res.status(200).json({
+        success: true,
+        msg: "Valid User",
+        data: req.user,
+    })
+});
 
 
 
