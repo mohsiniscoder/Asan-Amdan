4c17c4241cd521aa732be7b0b3e3d52437eddd8b
MahrAbrarThind <mahrabrarthind@gmail.com>
2024-12-24 22:18:43 +0500

    added controller for pending gigs

--- server/Routes/GigRoutes/gigRoutes.js
+++ server/Routes/GigRoutes/gigRoutes.js
@@ -1,11 +1,12 @@
 import express from "express"
 const router=express.Router();
-import { createGigController, deleteGigController, getAllGigsController, getGigByIdController, getGigForServiceProviderController, updateGigController } from "../../Controllers/GigControllers/gigController.js";
-import { authenticateServiceProvider } from "../../Middlewares/authMiddlewares.js";
+import { createGigController, deleteGigController, getAllGigsController, getGigByIdController, getGigForServiceProviderController, getPendingGigsController, updateGigController } from "../../Controllers/GigControllers/gigController.js";
+import { authenticateServiceProvider, checkAdmin } from "../../Middlewares/authMiddlewares.js";
 
 router.post('/createGig/:serviceProviderId',authenticateServiceProvider,createGigController);
 router.get('/getAllGigs',getAllGigsController)
 router.get('/getServiceProviderGigs/:serviceProviderId',authenticateServiceProvider,getGigForServiceProviderController)
+router.get('/getPendingGigs',checkAdmin,getPendingGigsController);
 router.get('/getGigById/:gigId',getGigByIdController);
 router.put('/updateGig/:gigId',authenticateServiceProvider,updateGigController);
 router.delete('/deleteGig/:gigId',authenticateServiceProvider,deleteGigController);
