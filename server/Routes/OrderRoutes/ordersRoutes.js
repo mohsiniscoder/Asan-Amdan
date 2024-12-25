cc2061fb1574c05b5074299b203be87533518692
MahrAbrarThind <mahrabrarthind@gmail.com>
2024-12-25 17:40:22 +0500

    solved error in backend

--- server/Routes/OrderRoutes/ordersRoutes.js
+++ server/Routes/OrderRoutes/ordersRoutes.js
@@ -10,7 +10,7 @@ router.get("/getNonTechOrders",getNonTechOrdersController);
 router.get("/getClientOrders/:id",checkUser,getClientOrdersController);
 router.get("/getServiceProviderOrders/:id",authenticateServiceProvider,getServiceProviderOrdersController);
 router.post("/addOrder",checkUser,addOrderController);
-router.put("/updateOrderStatus/:id", updateOrderStatusController);
+// router.put("/updateOrderStatus/:id", updateOrderStatusController);
 router.put("/updateClientOrderStatus/:id",checkUser,updateClientOrderStatus); 
 router.put("/updateServiceProviderOrderStatus/:id",authenticateServiceProvider,updateServiceProviderOrderStatus); 
 export default router;
