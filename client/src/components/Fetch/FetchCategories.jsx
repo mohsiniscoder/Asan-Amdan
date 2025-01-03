fcaced1f18a2ec08c98cadc762bb8f6bb4ea74c8
Mohsin Shahbaz <96002185+mohsiniscoder@users.noreply.github.com>
2025-01-03 16:50:44 +0500

    Update FetchCategories.jsx url

--- client/src/components/Fetch/FetchCategories.jsx
+++ client/src/components/Fetch/FetchCategories.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const fetchCategories = async () => {
   try {
-    const response = await axios.get("http://localhost:4000/api/v1/categories/getAllCategories");
+    const response = await axios.get("https://asan-amdan-py4u.vercel.app/api/v1/categories/getAllCategories");
     return response.data.categories;  // Return categories directly
   } catch (error) {
     console.error("Error fetching categories:", error.response?.data?.msg || error.message);
